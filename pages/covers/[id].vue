56d8673bb06c87e3c58fe72b2719fd7046dea3d0
Gokhan <gokhan@sylow.net>
2024-09-26 12:45:07 +0200

    Add transition effects

--- pages/covers/[id].vue
+++ pages/covers/[id].vue
@@ -7,6 +7,7 @@
   const coverStore = useCoverStore()
   const cover = computed(() => coverStore.get(Number(route.params.id)))
 
+  const tabToShow = ref('cover')
   const api = useCoverApi()
   const run = async (id: number) => {
     if (cover.value?.aasm_state != 'created')
@@ -18,74 +19,98 @@
 
 <template>
   <div>
-    <section class="hero" v-if="cover?.aasm_state == 'completed'">
+    <section class="hero">
       <div class="hero-body">
         <p class="title">Cover Letter Details</p>
         <p class="subtitle">View and manage your cover letter.</p>
       </div>
     </section>
-    <section class="hero is-warning is-light" v-else>
-      <div class="hero-body">
-        <p class="title">Cover Letter</p>
-        <p class="subtitle"><br>
-           <span v-if="cover?.aasm_state == 'running'">Generate a cover letter for the job description below.</span>
-           <button class="button" @click="run(cover.id)" v-if="cover?.aasm_state == 'created'">
-            <span class="icon">
-              <i class="fa-solid fa-play"></i>
-            </span>
-            <span>Click here to generate cover letter (1 credit)</span>
-          </button>
-          <br/>.
-        </p>
-      </div>
-    </section>
-
     <div class="container">
-      <div  v-if="cover">
+      <div class="tabs ">
+        <ul>
+          <li :class="{'is-active': tabToShow == 'cover'}" @click="tabToShow ='cover'">
+            <a>
+              <span class="icon is-small"><i class="fa-solid fa-envelope-open-text"></i></span>
+              <span>Cover</span>
+            </a>
+          </li>
+          <li :class="{'is-active': tabToShow == 'job'}" @click="tabToShow ='job'">
+            <a>
+              <span class="icon is-small"><i class="fa-solid fa-user-doctor"></i></span>
+              <span>Job Description</span>
+            </a>
+          </li>
+          <li :class="{'is-active': tabToShow == 'resume'}" @click="tabToShow ='resume'">
+            <a>
+              <span class="icon is-small"><i class="fa-solid fa-file"></i></span>
+              <span>Your Resume</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div v-if="tabToShow=='cover'">
         <Transition name="fade" mode="out-in">
-          <div v-if="cover.cover" key="cover-content" class="cover-content">
-            <div style="white-space: pre-line" class="box">
-              {{ cover.cover }}
+          <div v-if="cover?.cover">
+            <div v-if="cover.cover" key="cover-content" class="cover-content">
+              <div style="white-space: pre-line" class="box">
+                {{ cover.cover }}
+              </div>
             </div>
           </div>
+          <div v-else>
+            <br/>
+            <span v-if="cover?.aasm_state == 'running'">Writing the cover letter now. It will show here when completed.</span>
+            <button class="button" @click="run(cover.id)" v-if="cover?.aasm_state == 'created'">
+              <span class="icon">
+                <i class="fa-solid fa-play"></i>
+              </span>
+              <span>Click here to generate cover letter (1 credit)</span>
+            </button>
+          </div>
         </Transition>
-        <hr/>
-        <h3 class="title is-3">Job Description</h3>
-        <div style="white-space: pre-line" class="notification box">
-          {{ cover.job_description }}
-        </div>
-        <hr/>
-        <h3 class="title is-3">Resume</h3>
-        <div style="white-space: pre-line" class="notification box">
-          {{ cover.resume_content }}
+      </div>
+
+      <div v-if="tabToShow=='job'">
+        <div style="white-space: pre-line" class="box">
+          {{ cover?.job_description }}
         </div>
       </div>
-      <div class="notification is-danger" v-else>
-        <h2 class="title is-2">Oppsss</h2>
-        <p>You dont have such a cover letter</p>
+
+      <div v-if="tabToShow=='resume'">
+        <div style="white-space: pre-line" class="box">
+          {{ cover?.resume_content }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
-.loading-placeholder,
+<style scoped lang="scss">
 .cover-content,
-.not-found {
-  min-height: 200px; /* Adjust as needed */
+.content-box {
+  min-height: 200px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 100%;
 }
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 1s ease;
+  transition: all 0.5s ease;
 }
 
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+  transform: translateY(30px);
+}
+
+.content-box {
+  background-color: #f5f5f5;
+  border-radius: 8px;
+  padding: 20px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
 }
 </style>
